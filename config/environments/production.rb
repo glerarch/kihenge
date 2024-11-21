e736d8ae2d54953988f29eab82185a56ac3edd52
c3nsored <u.gabrielr@gmail.com>
2024-11-21 16:24:37 +0100

    fix: prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,10 +88,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "kihenge.onrender.com"
-  ]
-  
+  config.hosts << "kihenge.onrender.com"
+    
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
